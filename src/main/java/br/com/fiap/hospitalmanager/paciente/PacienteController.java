ef07db090c1990c473703e129a0def6aae64ad28
FilipeJava <phill.java@gmail.com>
2023-10-23 10:34:01 -0300

    Crud Finalizado

--- src/main/java/br/com/fiap/hospitalmanager/paciente/PacienteController.java
+++ src/main/java/br/com/fiap/hospitalmanager/paciente/PacienteController.java
@@ -1,5 +1,7 @@
 package br.com.fiap.hospitalmanager.paciente;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -9,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -58,6 +61,37 @@ public class PacienteController {
         service.save(paciente);
 
        return"redirect:/paciente";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        Optional<Paciente> paciente = service.findById(id);
+
+        // Verifique se o paciente foi encontrado antes de adicionar ao modelo
+        if (paciente.isPresent()) {
+            model.addAttribute("paciente", paciente.get());
+        } else {
+            // Redirecione para uma página de erro ou lide com a situação de paciente não encontrado
+            model.addAttribute("error", "Paciente não encontrado");
+            return "redirect:/paciente";            
+        }
+
+        return "paciente/update-form";
+    }
+
+      @PostMapping("/update/{id}")
+    public String updatePaciente(@PathVariable("id") Long id, @Valid Paciente paciente,
+                                BindingResult result, RedirectAttributes redirect) {
+        if (result.hasErrors()) {
+            return "/paciente/update-form";
+        }
+
+        // Define o ID do paciente para garantir que você está atualizando o paciente correto
+        paciente.setId(id);
+        service.save(paciente);
+
+        redirect.addFlashAttribute("success", "Paciente atualizado com sucesso");
+        return "redirect:/paciente";
     }
 
 
